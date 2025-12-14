bec7e738d2e93c837a1424b1c7685836437904dc
MasakiMichida <249415391+MasakiMichida@users.noreply.github.com>
2025-12-14 15:45:14 +0900

    chor(api): scaffold Spring Boot Project add/health endpoint

---
+++ api/src/test/java/io/github/homeinventory/HomeInventoryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.homeinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeInventoryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
